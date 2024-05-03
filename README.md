03f85afaa41494689677bd516793afcbe1ffdcca
Demarquies Jolley <ahmir1128@gmail.com>
2024-05-03 00:13:06 -0500

    last minute

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Demarquies Jolley
 
-## Review 
+## Review
 
 We learned how to create a test for testing our code. We learned more of what node is for & can do We learned why the tests are important.
 We also learned some postgres to help out with reading databases. I added a collection class.
@@ -10,3 +10,17 @@ We also learned some postgres to help out with reading databases. I added a coll
 ## UML
 
 [Whiteboard](./CC3.png)
+
+## Lab 04
+
+Who was your partner?
+
+- Armen, Wajiha
+
+What was your key takeaway?
+
+- Make sure you leave simple clear notes in your code so it will be easier to work with.
+
+Share the link to your PR request.
+
+- <https://github.com/Djolley1/api-server/pulls>
--- src/models/index.js
+++ src/models/index.js
@@ -4,13 +4,16 @@
 const Collection = require('./collection'); // Import the Collection class
 const PeopleModel = require('./people');
 const FoodModel = require('./food');
+const ClothesModel = require('./clothes');
 
 // let sequelizeOptions = {
 //   dialect: process.env.NODE_ENV === 'test' ? 'sqlite' : 'postgres', // Assuming PostgreSQL for non-test environments
 // };
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory' : process.env.DATABASE_URL;
 console.log('Database URL:', DATABASE_URL);
+
 const { Sequelize, DataTypes } = require('sequelize');
+
 let sequelize = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
@@ -24,13 +27,16 @@ let sequelize = new Sequelize(DATABASE_URL, {
 
 const People = PeopleModel(sequelize, DataTypes);
 const Food = FoodModel(sequelize, DataTypes);
+const Clothes = ClothesModel(sequelize, DataTypes);
 
 // Create instances of Collection class for each model
 const peopleCollection = new Collection(People);
 const foodCollection = new Collection(Food);
+const clothesCollection = new Collection(Clothes);
 
 module.exports = {
   db: sequelize,
   People: peopleCollection,
   Food: foodCollection,
+  Clothes: clothesCollection,
 };
